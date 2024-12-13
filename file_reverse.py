de4d45dd5d30f6ff134e5def8d37ac25fbb8533a
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2024-12-13 14:49:53 +0530

    Add files via upload

---
+++ file_reverse.py
@@ -0,0 +1,7 @@
+f=open(r"C:\Users\Jameel\Desktop\Python\demo.txt","r") 
+line=f.read()
+print(f"The data in file before reversing is :{line}")
+line1=line[::-1]
+print(f"\nThe data in the file after reverseing is :{line1}")
+
+    
